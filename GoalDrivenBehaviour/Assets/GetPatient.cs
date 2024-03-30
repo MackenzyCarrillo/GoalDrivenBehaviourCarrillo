579e6c94cef10695eb36133e8cc8c80083aa14f5
MackenzyCarrillo <709666@student.dusd.net>
2024-03-30 11:08:33 -0700

    finished Adding More Resources to the World

--- GoalDrivenBehaviour/Assets/GetPatient.cs
+++ GoalDrivenBehaviour/Assets/GetPatient.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class GetPatient : GAction
 {
+    GameObject resource;
     public override bool PrePerform()
     {
+        target = GWorld.Instance.RemovePatient();
+        if(target == null )
+        {
+            return false;
+        }
+
+        resource = GWorld.Instance.RemoveCubicl3();
+        if( resource != null )
+        {
+            inventory.AddUtem( resource );
+        }
+        else
+        {
+            GWorld.Instance.AddPatient(target);
+            target = null;
+            return false;
+        }
+       
+        GWorld.Instance.GetWorld().ModifyState("FreeCubicle",-1);
         return true;
     }
 
     public override bool PostPerform()
     {
+        GWorld.Instance.GetWorld().ModifyState("Waiting", -1);
+        if (target)
+            target.GetComponent<GAgent>().inventory.AddUtem(resource);
+        
+            
+        
         return true;
     }
 
